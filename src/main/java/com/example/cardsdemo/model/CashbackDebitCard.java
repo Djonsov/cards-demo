0fcd467e5f7c2d0044b363e874fb28001cd4b207
Djonsov <djonsov@mail.ru>
2023-08-10 18:34:58 +0300

    fix

--- src/main/java/com/example/cardsdemo/model/CashbackDebitCard.java
+++ src/main/java/com/example/cardsdemo/model/CashbackDebitCard.java
@@ -1,2 +1,28 @@
-package com.example.cardsdemo.model;public class CashbackDebitCard {
+package com.example.cardsdemo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Класс описывает сущность "Дебетовая карта с программой кешбека"
+ */
+@Setter
+@Getter
+public final class CashbackDebitCard extends DebitCard {
+    private static final Double cashBackMultiplier = 0.05;
+    public CashbackDebitCard() {
+        super();
+    }
+
+    /**
+     * Списание со счета
+     * @param amount - количество списываемых со счета денежных средств
+     * @return - результат выполнения операции. True - успешное выполнение, false - неуспешное выполнение
+     */
+    @Override
+    public Boolean pay(Double amount) {
+        if (amount >= 5000)
+            setBalance(getBalance() + amount * cashBackMultiplier);
+        return super.pay(amount);
+    }
 }
